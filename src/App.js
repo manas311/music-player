45abf8caf7f5ceb92e958c5e3b53937de807ad23
Manas Tewari <manastewari311@gmail.com>
2022-11-15 10:46:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-function App() {
-  return (
-    <>
+import { useState, useEffect } from "react";
+import Player from "./components/Player";
+import data from './data.json';
 
-    </>
+function App() {
+  const [songs] = useState(data);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
+  return (
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
+    </div>
   );
 }
 
